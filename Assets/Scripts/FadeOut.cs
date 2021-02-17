d2f03ec856fb32ebc00c382a16807445ee237abe
EvilMojo <mrmojo42@hotmail.com>
2021-02-18 00:05:19 +1100

    Update 18/02/21
    Added executable and escape-to-quit functionality

--- Assets/Scripts/FadeOut.cs
+++ Assets/Scripts/FadeOut.cs
@@ -19,26 +19,32 @@ public class FadeOut : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown ("1")) {
-			this.gameObject.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, -4.5f);
-			core.go (1);
-			active = true;
-		} else if (Input.GetKeyDown ("2")) {
-			this.gameObject.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, -4.5f);
-			core.go (2);
-			active = true;
-		} else if (Input.GetKeyDown ("3")) {
-			this.gameObject.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, -4.5f);
-			core.go (3);
-			active = true;
-		} else if (Input.GetKeyDown ("4")) {
-			this.gameObject.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, -4.5f);
-			core.go (4);
-			active = true;
-		} else if (Input.GetKeyDown ("5")) {
-			this.gameObject.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, -4.5f);
-			core.go (5);
-			active = true;
+		if (Input.GetKey(KeyCode.Escape)) {
+			Application.Quit();
+		}
+
+		if (!active) {
+			if (Input.GetKeyDown ("1")) {
+				this.gameObject.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, -4.5f);
+				core.go (1);
+				active = true;
+			} else if (Input.GetKeyDown ("2")) {
+				this.gameObject.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, -4.5f);
+				core.go (2);
+				active = true;
+			} else if (Input.GetKeyDown ("3")) {
+				this.gameObject.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, -4.5f);
+				core.go (3);
+				active = true;
+			} else if (Input.GetKeyDown ("4")) {
+				this.gameObject.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, -4.5f);
+				core.go (4);
+				active = true;
+			} else if (Input.GetKeyDown ("5")) {
+				this.gameObject.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, -4.5f);
+				core.go (5);
+				active = true;
+			}
 		}
 
 		if (active && !reverseFade && transparency > 0.0f) {
